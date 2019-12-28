890bdfe9de3081824a3fe3e67fce0a53b3b23365
surajt <smartsurajit2008@gmail.com>
2019-12-28 17:41:18 +0300

    removed unnecessary files

--- Resume/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resume.settings')
-
-app = Celery('Resume')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
